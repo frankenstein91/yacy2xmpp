2b22802e9b33e03cfdb980433f890b441dd8fb3f
Frankenstein91 <frankenstein91.info@googlemail.com>
2016-02-28 17:09:58 +0100

    Eine Suche ist jetzt möglich. Der Bot wird über eine zweite Python Datei gestartet. bot = YacyJabberBot(username, password) bot.serve_forever()

--- yacyBot.py
+++ yacyBot.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python2
 from jabberbot import JabberBot, botcmd
 import datetime, json
+import logging
+from urllib import urlopen
 #der Patch https://raw.githubusercontent.com/freebsd/freebsd-ports/master/net-im/py-xmpppy/files/patch-xmpp-transports.py muss sein.
 
 class YacyJabberBot(JabberBot):
+    logging.basicConfig()
 
     MSG_AUTHORIZE_ME = "Bitte fuege mich deiner Kontaktliste hinzu und ich werde es auch tun."
 
@@ -22,5 +25,14 @@ class YacyJabberBot(JabberBot):
     @botcmd
     def suchen(self,mess,args):
         """Bei Yacy suchen"""
-        return str("todo")
+        YacyServer = "http://search.yacy.de"
+        url = urlopen(YacyServer + "/yacysearch.json?query=" + args).read()
+        suche = json.loads(url)
+        raus = "\n" + suche["channels"][0]["title"] + "\n\n"
+        for item in suche["channels"][0]["items"][:10]:
+            raus = raus + item["title"] + "\n" + item["link"] + "\n Size: " + item["sizename"]
+            raus = raus + "\n Pup. Date: " + item["pubDate"] + "\n\n"
+        f = open("debug2.txt", "w")
+        f.write(raus.encode('utf-8'))
+        return str(raus.encode('utf-8'))
 
